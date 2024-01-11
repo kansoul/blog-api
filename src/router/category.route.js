0f1d2208244cfb1b8e7a48a9611a0952215ed7a4
Kozocom-DoanHV <doanhv@kozo-japan.com>
2024-01-11 23:45:19 +0700

    category

--- src/router/category.route.js
+++ src/router/category.route.js
@@ -6,6 +6,7 @@ const {
 } = require("../controller/category.controller");
 const validation = require("../middleware/validation");
 const { categorySchema } = require("../schemas/category.schema");
+const auth = require("../middleware/auth");
 const route = express();
 
 // GET
@@ -13,6 +14,6 @@ route.get("/categories", getCategories);
 route.get("/category/:slug", getCategory);
 
 // POST
-route.post("/category", validation(categorySchema), postCategory);
+route.post("/category", auth, validation(categorySchema), postCategory);
 
 module.exports = route;
